fac665774e99fe6f06d8b07471f3efa7a6a29ba5
TheVitik <lokplay2003@gmail.com>
2022-10-21 23:12:26 +0300

    Render top tags data from repository

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\SearchesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-
     /**
      * Just to render something at the main page.
      *
+     * @param SearchesRepository $repository
      * @return Response
      */
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(SearchesRepository $repository): Response
     {
-        return $this->render(
-            'index.html.twig'
-        );
+        $topTags = $repository->getTopTags();
+
+        return $this->render('index.html.twig', [
+            'topTags' => $topTags
+        ]);
     }
 }
