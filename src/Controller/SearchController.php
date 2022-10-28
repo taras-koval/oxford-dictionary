3107b5da8762732ac977f74a5b62e879be5b3f7e
Vladyslav Kuzmenko <58442387+kusem@users.noreply.github.com>
2022-10-28 12:29:50 +0300

    Add search suggest + small fixes (#12)
    * Fix front
    Improve Search
    Add search suggest
    
    * Codestyle fixes
    
    * Move JS to webpack
    Codestyle fixes
    README update for Redis server start

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,16 +2,43 @@
 
 namespace App\Controller;
 
+use App\Repository\SearchesRepository;
 use App\Service\OxfordDictionary\OxfordDictionary;
 use App\Service\OxfordDictionary\OxfordDictionaryException;
 use App\Service\SearchesService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
+    /**
+     * Returns array of possible words with same beginning
+     *
+     * @param Request $request
+     * @param SearchesRepository $search
+     * @return JsonResponse
+     */
+    #[Route('/quick-search', name: 'quick_search')]
+    public function quickSearch(Request $request, SearchesRepository $search): JsonResponse
+    {
+        $wordBegin = $request->query->get('wordBegin');
+        if (strlen($wordBegin) > 2) {
+
+            return new JsonResponse([
+                'status'    => 1,
+                'matches'   => $search->getFastSearchMatchWords($wordBegin)
+            ]);
+        }
+
+        return new JsonResponse([
+            'status' => 0,
+            'message'=> 'At least 3 symbols required for search initiation.'
+        ]);
+    }
+
     /**
      * Search for results and render page
      *
@@ -22,17 +49,17 @@ class SearchController extends AbstractController
     {
         $word = $request->get('q');
         $entries = [];
-        
+
         if (!empty($word)) {
             $word = strtolower($word);
-    
+
             $lemma = $dictionary->lemma($word);
             if ($lemma) {
                 $word = $lemma->getInflectionOf();
             }
-            
+
             $searchService->addSearch($word);
-    
+
             $entries = $dictionary->entries($word);
         }
 
