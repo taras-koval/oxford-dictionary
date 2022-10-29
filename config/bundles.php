04e0b72f98d5ac6322546ae04097074aed832fed
Tanya Fedoriv <tanya.fedoriv25@gmail.com>
2022-10-29 09:56:52 +0300

    Create admin dashboard. Add users menu item

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\HiddenField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserCrudController extends AbstractCrudController
+{
+    /** @var UserPasswordHasherInterface */
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            HiddenField::new('id'),
+            TextField::new('name'),
+            EmailField::new('email'),
+            ArrayField::new('roles'),
+            FormField::addPanel('Change password')->setIcon('fa fa-key'),
+            Field::new('password', 'New password')->onlyOnForms()
+                ->setFormType(RepeatedType::class)
+                ->setFormTypeOptions([
+                    'type' => PasswordType::class,
+                    'first_options' => ['label' => 'New password'],
+                    'second_options' => ['label' => 'Repeat password'],
+                ]),
+        ];
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+
+        $this->addHashPasswordEventListener($formBuilder);
+
+        return $formBuilder;
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+
+        $this->addHashPasswordEventListener($formBuilder);
+
+        return $formBuilder;
+    }
+
+    /**
+     * @required
+     */
+    public function setHasher(UserPasswordHasherInterface $passwordEncoder): void
+    {
+        $this->passwordHasher = $passwordEncoder;
+    }
+
+    protected function addHashPasswordEventListener(FormBuilderInterface $formBuilder)
+    {
+        $formBuilder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            /** @var User $user */
+            $user = $event->getData();
+            if ($user->getPassword()) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            }
+        });
+    }
+}
+
